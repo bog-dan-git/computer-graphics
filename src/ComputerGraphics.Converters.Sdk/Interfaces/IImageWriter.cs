f9383b1d38d811e8178c6345cf024ac1b21e01e5
Bogdan Dobryanskiy <godzgiftplay@gmail.com>
2021-03-21 07:26:56 +0200

    feat: update interfaces, implement jpeg

--- src/ComputerGraphics.Converters.Sdk/Interfaces/IImageWriter.cs
+++ src/ComputerGraphics.Converters.Sdk/Interfaces/IImageWriter.cs
@@ -1,11 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using ComputerGraphics.Converters.Sdk.Model;
 
 namespace ComputerGraphics.Converters.Sdk.Interfaces
 {
     public interface IImageWriter
     {
-        Task WriteAsync(string path, List<RgbColor> colors);
+        Task WriteAsync(string path, RgbColor[,] colors);
     }
 }
---
+++ src/ComputerGraphics.Jpeg/Models/BitCode.cs
@@ -0,0 +1,8 @@
+﻿namespace ComputerGraphics.Jpeg.Models
+{
+    public struct BitCode
+    {
+        public ushort Code { get; set; }
+        public byte AmountOfBits { get; set; }
+    }
+}
