5fa98941c57965aed94caf9434f8e962d3015b1e
SavStanis <42719022+SavStanis@users.noreply.github.com>
2021-04-06 01:05:31 +0300

    Main models were added

---
+++ src/ComputerGraphics.ObjLoader/Models/Triangle.cs
@@ -0,0 +1,9 @@
+﻿namespace ComputerGraphics.ObjLoader.Models
+{
+    public class Triangle
+    {
+        public Vector3 A { get; set; }
+        public Vector3 B { get; set; }
+        public Vector3 C { get; set; }
+    }
+}
