7a7ff1f37435ede3511738e7e482a327a1092ae7
Stanislav Savastru <42719022+SavStanis@users.noreply.github.com>
2021-06-02 02:19:44 +0300

    feat: add cameras (#8)

--- src/ComputerGraphics.RayTracing.Core/Entities/Cameras/Camera.cs
+++ src/ComputerGraphics.RayTracing.Core/Entities/Cameras/Camera.cs
@@ -1,8 +1,15 @@
-﻿namespace ComputerGraphics.RayTracing.Core.Entities.Cameras
+﻿using System.Numerics;
+
+namespace ComputerGraphics.RayTracing.Core.Entities.Cameras
 {
-    public class Camera
+    public abstract class Camera
     {
         public int Id { get; set; }
         public Transform Transform { get; set; }
+        public Vector3 Origin { get; } = Vector3.Zero;
+        public Vector3 Direction { get; } = new(0, 0, -1);
+        
+        public abstract Ray GetRay(float x, float y);
+        
     }
 }
--- src/ComputerGraphics.RayTracing.Core/Entities/Cameras/PerspectiveCamera.cs
+++ src/ComputerGraphics.RayTracing.Core/Entities/Cameras/PerspectiveCamera.cs
@@ -1,7 +1,60 @@
-﻿namespace ComputerGraphics.RayTracing.Core.Entities.Cameras
+﻿using System;
+using System.Numerics;
+using ComputerGraphics.Common;
+
+namespace ComputerGraphics.RayTracing.Core.Entities.Cameras
 {
     public class PerspectiveCamera : Camera
     {
-        public float Fov { get; set; }
+        private readonly Vector3 _lowerLeftCorner;
+        private readonly double _viewportWidth;
+        private readonly double _viewportHeight;
+        
+        public float HFov { get; set; } 
+
+        private Matrix4x4? _rotationMatrix;
+        private Matrix4x4? _transitionMatrix;
+        public PerspectiveCamera(float hFov, float aspectRatio)
+        {
+            HFov = hFov;
+            var hFovRad = hFov * MathF.PI / 180;
+            
+            _viewportHeight = 2.0 * MathF.Tan(hFovRad / 2);
+            _viewportWidth = aspectRatio * _viewportHeight;
+
+            _lowerLeftCorner = Origin - new Vector3(0, (float) (_viewportHeight / 2), 0) 
+                                      - new Vector3((float) (_viewportWidth / 2), 0, 0)
+                               + Direction;
+        }
+        public override Ray GetRay(float x, float y)
+        {
+            var rayDir = new Vector3(
+                (float) (_lowerLeftCorner.X + x * _viewportWidth),
+                (float) (_lowerLeftCorner.Y + y * _viewportHeight),
+                0) + Direction;
+
+            return new Ray(
+                Vector3.Transform(Origin, GetTransitionMatrix()),
+                Vector3.Normalize(Vector3.Transform(rayDir, GetRotationMatrix()))
+                );
+        }
+
+        private Matrix4x4 GetRotationMatrix()
+        {
+            _rotationMatrix ??= new TransposedTransformationMatrixBuilder()
+                .Rotate(Transform.Rotation)
+                .Build();
+
+            return _rotationMatrix.Value;
+        }
+
+        private Matrix4x4 GetTransitionMatrix()
+        {
+            _transitionMatrix ??= new TransposedTransformationMatrixBuilder()
+                .Move(Transform.Position)
+                .Build();
+
+            return _transitionMatrix.Value;
+        }
     }
 }
